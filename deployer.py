e352c85b47f35f43700a0372a8139de56a92ef98
Mohamed Attahri <mohamed@attahri.com>
2014-02-12 13:11:35 +0000

    fixed and generalization of relative paths

--- deployer.py
+++ deployer.py
@@ -1,19 +1,18 @@
 import os
+import sys
 import json
 from flask import Flask, request
 from subprocess import call
 
 app = Flask(__name__)
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DEPLOY_SCRIPT = os.path.join(BASE_DIR, 'deploy.sh')
+DEPLOY_SCRIPT = os.path.dirname(os.path.abspath(__file__)) + '/deploy.sh'
 
 @app.route('/', methods=['GET', 'POST'])
 def deploy():
 	if request.method == 'GET':
 		return 'Deployment server is up.'
 
-	data = json.loads(request.data)
 	call([DEPLOY_SCRIPT, '/home/web-ma'])
 
 if __name__ == '__main__':
-	app.run(port=6868)
+ 	app.run(port=6868, debug=True)
