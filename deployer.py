7da7268f7ebe1da0fddd10f36b79c0972309f244
Mohamed Attahri <mohamed@attahri.com>
2014-02-12 12:48:13 +0000

    fixes

--- deployer.py
+++ deployer.py
@@ -1,13 +1,19 @@
+import os
 import json
 from flask import Flask, request
 from subprocess import call
 
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DEPLOY_SCRIPT = os.path.join(BASE_DIR, 'deploy.sh')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def deploy():
+	if request.method == 'GET':
+		return 'Deployment server is up.'
+
 	data = json.loads(request.data)
-	call(['deploy.sh', '/home/web-ma'])
+	call([DEPLOY_SCRIPT, '/home/web-ma'])
 
 if __name__ == '__main__':
 	app.run(port=6868)
